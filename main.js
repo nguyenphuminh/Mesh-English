fb9e46bef0c44a0cfecc11f563161da48128202e
Nguyen Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2020-06-26 11:14:03 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,294 @@
+//Loading
 topbar.show();
 setTimeout(
 	function(){
 	topbar.hide();
 	}
 ,1200);
+var point=0;
+
+function correct1(){
+	var a = document.querySelectorAll(".ans1");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong1(){
+	var a = document.querySelectorAll(".ans1");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct2(){
+	var a = document.querySelectorAll(".ans2");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong2(){
+	var a = document.querySelectorAll(".ans2");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct3(){
+	var a = document.querySelectorAll(".ans3");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong3(){
+	var a = document.querySelectorAll(".ans3");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct4(){
+	var a = document.querySelectorAll(".ans4");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong4(){
+	var a = document.querySelectorAll(".ans4");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct5(){
+	var a = document.querySelectorAll(".ans5");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong5(){
+	var a = document.querySelectorAll(".ans5");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct6(){
+	var a = document.querySelectorAll(".ans6");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong6(){
+	var a = document.querySelectorAll(".ans6");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct7(){
+	var a = document.querySelectorAll(".ans7");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong7(){
+	var a = document.querySelectorAll(".ans7");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct8(){
+	var a = document.querySelectorAll(".ans8");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong8(){
+	var a = document.querySelectorAll(".ans8");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct9(){
+	var a = document.querySelectorAll(".ans9");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong9(){
+	var a = document.querySelectorAll(".ans9");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct10(){
+	var a = document.querySelectorAll(".ans10");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong10(){
+	var a = document.querySelectorAll(".ans10");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+
+function correct11(){
+	var a = document.querySelectorAll(".ans11");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong11(){
+	var a = document.querySelectorAll(".ans11");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct12(){
+	var a = document.querySelectorAll(".ans12");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong12(){
+	var a = document.querySelectorAll(".ans12");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct13(){
+	var a = document.querySelectorAll(".ans13");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong13(){
+	var a = document.querySelectorAll(".ans13");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct14(){
+	var a = document.querySelectorAll(".ans14");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong14(){
+	var a = document.querySelectorAll(".ans14");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct15(){
+	var a = document.querySelectorAll(".ans15");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong15(){
+	var a = document.querySelectorAll(".ans15");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct16(){
+	var a = document.querySelectorAll(".ans16");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong16(){
+	var a = document.querySelectorAll(".ans16");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct17(){
+	var a = document.querySelectorAll(".ans17");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong17(){
+	var a = document.querySelectorAll(".ans17");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct18(){
+	var a = document.querySelectorAll(".ans18");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong18(){
+	var a = document.querySelectorAll(".ans18");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct19(){
+	var a = document.querySelectorAll(".ans19");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong19(){
+	var a = document.querySelectorAll(".ans19");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function correct20(){
+	var a = document.querySelectorAll(".ans20");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+	point+=1;
+}
+function wrong20(){
+	var a = document.querySelectorAll(".ans20");
+	for (i=0;i<a.length;i++){
+		a[i].setAttribute("disabled", "");
+	}
+}
+
+function submit(){
+	var b=document.querySelector(".points");
+	b.innerHTML = "Điểm:" + "  " + point;
+}
